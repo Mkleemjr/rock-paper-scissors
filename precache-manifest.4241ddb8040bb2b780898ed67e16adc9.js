39adfca8b746bc6b994247c51cc3e2273c09bd33
mkleemoff <mkleemjr@gmail.com>
2020-03-06 17:58:59 -0500

    Updates

---
+++ precache-manifest.4241ddb8040bb2b780898ed67e16adc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f473a86d3eb7e7d648f5ed9873aa42",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "4974ab0faefe768138e8",
+    "url": "/rock-paper-scissors/static/css/main.17a385d8.chunk.css"
+  },
+  {
+    "revision": "eb755750511386e2c0c3",
+    "url": "/rock-paper-scissors/static/js/2.45b4b242.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rock-paper-scissors/static/js/2.45b4b242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4974ab0faefe768138e8",
+    "url": "/rock-paper-scissors/static/js/main.308f68b6.chunk.js"
+  },
+  {
+    "revision": "9dd383734fa0f44644d7",
+    "url": "/rock-paper-scissors/static/js/runtime-main.633d143a.js"
+  }
+]);
